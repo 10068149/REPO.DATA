fd6c0efdb54db15eecea3cbf9456d6f1970ce8ca
10068149 <nash.anthony.c@gmail.com>
2017-01-08 08:32:39 -0800

    Neote: Initial Sycn . . .

---
+++ ChessGame.cpp
@@ -0,0 +1,83 @@
+//
+//  ChessGame.cpp
+//  PORJECT.dprokurat@google
+//
+//  Created by Anthony Nash on 1/5/17.
+//  Copyright © 2017 me. All rights reserved.
+//
+
+
+
+// all includes here
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+// class definition here
+// Reporter
+class Reporter {
+    
+public:
+    //code here...
+    
+private:
+    //code here...
+    
+    
+};
+
+//1. class Game. this will be our main entity. this is what we are building...
+
+class Game {
+    
+public:
+    //code here...
+    
+private:
+    //code here...
+    
+};
+
+//2. we need players ­ define class player...
+
+class Players {
+    
+public:
+    
+    
+private:
+    
+    //    Players one;
+    //    Players two;
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    
+    
+    
+    cout
+    << endl
+    << "Program ended with exit code: Game Over man!! game over!! "
+    << endl << endl;
+    return 0;
+    
+}
+
+// method implementations here
